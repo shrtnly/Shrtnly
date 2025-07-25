7b71577d63883d5ff49c9ca5ef88872ae089f0d7
shrtnly <shrtnly@gmail.com>
2025-07-25 14:29:15 +0600

    Updated LinkForm.tsx

--- src/components/LinkForm.tsx
+++ src/components/LinkForm.tsx
@@ -281,7 +281,7 @@ const LinkForm: React.FC<LinkFormProps> = ({ onLinkCreated }) => {
 
           {/* Custom Short Code */}
           <div className="md:col-span-3">
-            <label htmlFor="short_code" className="block text-sm sm:text-base font-medium text-gray-700 mb-2">
+            <label htmlFor="short_code" className="block text-sm sm:text-base font-medium text-white-700 mb-2">
              
             </label>
             <input
