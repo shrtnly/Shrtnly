1930ba81f7204b8975166b65fd411c84acd8a587
shrtnly <shrtnly@gmail.com>
2025-08-09 00:04:46 +0600

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -77,7 +77,7 @@ const HomePage: React.FC = () => {
   </div>
 </div>
       
-         <header style=”display: none”>
+         <header>
         <h1>Free URL Shortener with QR Codes & Lifetime Links</h1>
         <p>Create trackable short links with QR codes—100% free and never expire.</p>
       </header>
