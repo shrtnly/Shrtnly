58ce4b199ac132902371f94bc4b84c0096c1c304
shrtnly <shrtnly@gmail.com>
2025-08-11 14:19:35 +0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from './App.tsx';
 import './index.css';
 import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 );
