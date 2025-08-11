51c1c7b9d6182146d8bb146eeccdc7b55967b0b2
shrtnly <shrtnly@gmail.com>
2025-08-11 16:15:48 +0600

    Updated ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -63,7 +63,7 @@ const ContactPage: React.FC = () => {
       [e.target.name]: e.target.value
     }));
   };
-export default function HomePage() {
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
       <main className="container mx-auto px-4 py-8">
